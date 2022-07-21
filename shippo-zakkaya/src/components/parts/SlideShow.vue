7c0dac79c5c9f9cf863a8b7c5b6c6689edd319f1
litophoto <ciro17581@gmail.com>
2022-07-22 01:35:21 +0900

    .gitignore写真を無視するように変更。SlideShow.vue途中

---
+++ shippo-zakkaya/src/components/parts/SlideShow.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <img :src="getImageUrl(assetsImage)" alt=""> -->
+  <div class="row">
+    <div class="col-sm-4">
+      <img :src="getImageUrl(imageList[loopCounter-1])" alt="" class="img-fluid">
+    </div>
+    <div class="col-sm-4">
+      <img :src="getImageUrl(imageList[loopCounter])" alt="" class="img-fluid">
+    </div>
+    <div class="col-sm-4">
+      <img :src="getImageUrl(imageList[loopCounter + 1])" alt="" class="img-fluid">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const getImageUrl = (photoName) => {
+  return new URL(`../../assets/image/${photoName}`, import.meta.url).href
+}
+const assetsImage = "ec-site-sample2.jpeg"
+const imageList = [
+  "main01.jpeg",
+  "main02.jpeg",
+  "main03.jpeg",
+  "main05.jpeg",
+]
+const loopCounter = ref(0)
+</script>
+
+<style lang="scss">
+  
+</style>
